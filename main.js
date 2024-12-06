e47075de29a1a640e09e2d9a0e8ee0092e6cd4d8
Inform <Inform@kiev2.local>
2024-12-06 14:45:23 +0200

    Add stupid monkey; - add light to scene

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-import * as THREE from 'three';
-
-// const scene = new THREE.Scene();
+// import * as THREE from 'three';
+// import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+// // const scene = new THREE.Scene();
 // const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 // // const renderer = new THREE.WebGLRenderer();
@@ -21,27 +21,64 @@ import * as THREE from 'three';
 // // }
 // // renderer.setAnimationLoop(animate);
 
+// const renderer = new THREE.WebGLRenderer();
+// renderer.setSize( window.innerWidth, window.innerHeight );
+// document.body.appendChild( renderer.domElement );
+
+// const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 500 );
+// camera.position.set( 0, 0, 100 );
+// camera.lookAt( 0, 0, 0 );
+
+// const scene = new THREE.Scene();
+
+// // const material = new THREE.LineBasicMaterial( { color: 0x0000ff } );    
+// const points = [];
+// points.push( new THREE.Vector3( - 10, 0, 0 ) );
+// points.push( new THREE.Vector3( 0, 10, 0 ) );
+// points.push( new THREE.Vector3( 10, 0, 0 ) );
+// points.push( new THREE.Vector3( 0, - 10, 0 ) );
+// points.push( new THREE.Vector3( -10, -10, 0 ) );
+// points.push( new THREE.Vector3( 0, 0, 5 ) );
+// points.push( new THREE.Vector3( -10, 0, 0 ) );
+
+// const geometry = new THREE.BufferGeometry().setFromPoints( points );
+// // const line = new THREE.Line( geometry, material );
+// // scene.add( line );
+// // renderer.render( scene, camera );
+
+// const loader = new GLTFLoader();
+
+// loader.load( 'Blender/monkey.glb', async function ( gltf ) {
+//     const model = gltf.scene
+//     await renderer.compileAsync( model, camera, scene );
+// 	scene.add( model );
+//     renderer.render( scene, camera );
+// }, undefined, function ( error ) {
+
+// 	console.error( error );
+
+// } );
+
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+const scene = new THREE.Scene();
+scene.add(new THREE.AmbientLight( 0xffffff, 0.1) );
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.25, 20);
+camera.position.set( - 1.8, 0.6, 2.7 );
+var light = new THREE.PointLight( 0xffffff, 1);
+camera.add(light);
+
+const loader = new GLTFLoader();
+
+loader.load( './Blender/monkey.glb', async function ( gltf ) {
+    const model = gltf.scene;
+	scene.add( model );
+    renderer.render( scene, camera, model );
+}, undefined, function ( error ) {
+
+	console.error( error );
+
+} );
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
-
-const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 500 );
-camera.position.set( 0, 0, 100 );
-camera.lookAt( 0, 0, 0 );
-
-const scene = new THREE.Scene();
-
-const material = new THREE.LineBasicMaterial( { color: 0x0000ff } );    
-const points = [];
-points.push( new THREE.Vector3( - 10, 0, 0 ) );
-points.push( new THREE.Vector3( 0, 10, 0 ) );
-points.push( new THREE.Vector3( 10, 0, 0 ) );
-points.push( new THREE.Vector3( 0, - 10, 0 ) );
-points.push( new THREE.Vector3( -10, -10, 0 ) );
-points.push( new THREE.Vector3( 0, 0, 5 ) );
-points.push( new THREE.Vector3( -10, 0, 0 ) );
-
-const geometry = new THREE.BufferGeometry().setFromPoints( points );
-const line = new THREE.Line( geometry, material );
-scene.add( line );
-renderer.render( scene, camera );
