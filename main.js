c93fca12d2295380dd6e6b338585befb1e6d1259
Inform <Inform@kiev2.local>
2025-02-20 14:44:26 +0200

    modalPick

--- main.js
+++ main.js
@@ -62,51 +62,76 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { CSS2DRenderer, CSS2DObject } from 'three/addons/renderers/CSS2DRenderer.js';
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
 // const renderer = new THREE.WebGLRenderer();
-const renderer = new THREE.WebGLRenderer( { antialias: true } );
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+const labelRenderer = new CSS2DRenderer();
+labelRenderer.setSize(window.innerWidth, window.innerHeight);
+labelRenderer.domElement.style.position = 'absolute';
+labelRenderer.domElement.style.top = '0px';
+document.body.appendChild(labelRenderer.domElement);
+
 const canvas = renderer.domElement;
+
+const pointer = new THREE.Vector2();
+const raycaster = new THREE.Raycaster();
 class PickHelper {
-    constructor() {
-      this.raycaster = new THREE.Raycaster();
-      this.pickedObject = null;
-      this.pickedObjectSavedColor = 0;
+  constructor() {
+    this.raycaster = new THREE.Raycaster();
+    this.pickedObject = null;
+    this.pickedObjectSavedColor = 0;
+    this.molekulaDiv = 0;
+    this.molekulaLabel = 0;
+  }
+  pick(normalizedPosition, scene, camera, time) {
+    // restore the color if there is a picked object
+    if (this.pickedObject) {
+      this.pickedObject.material.emissive.setHex(this.pickedObjectSavedColor);
+      this.pickedObject = undefined;
     }
-    pick(normalizedPosition, scene, camera, time) {
-      // restore the color if there is a picked object
-      if (this.pickedObject) {
-        this.pickedObject.material.emissive.setHex(this.pickedObjectSavedColor);
-        this.pickedObject = undefined;
-      }
-   
-      // cast a ray through the frustum
-      this.raycaster.setFromCamera(normalizedPosition, camera);
-      // get the list of objects the ray intersected
-      const intersectedObjects = this.raycaster.intersectObjects(scene.children);
-      if (intersectedObjects.length) {
-        // pick the first object. It's the closest one
-        this.pickedObject = intersectedObjects[0].object;
-        // save its color
-        this.pickedObjectSavedColor = this.pickedObject.material.emissive.getHex();
-        // set its emissive color to flashing red/yellow
-        this.pickedObject.material.emissive.setHex(0xE7504E);
-        
-      }
+
+    // cast a ray through the frustum
+    this.raycaster.setFromCamera(normalizedPosition, camera);
+    // get the list of objects the ray intersected
+    const intersectedObjects = this.raycaster.intersectObjects(scene.children);
+    if (intersectedObjects.length) {
+      
+      // pick the first object. It's the closest one
+      this.pickedObject = intersectedObjects[0].object;
+      this.molekulaDiv = document.createElement('div');
+      this.molekulaDiv.className = 'label';
+      this.molekulaDiv.textContent = 'Moon';
+
+      this.molekulaDiv.style.backgroundColor = 'transparent';
+      this.molekulaDiv.style.color = 'black'
+      this.molekulaLabel = new CSS2DObject(this.molekulaDiv);
+      this.molekulaLabel.position.set(1.5 * 0, 0, 0);
+      this.molekulaLabel.center.set(0, 1);
+      this.pickedObject.add(this.molekulaLabel);
+      this.molekulaLabel.layers.set(0);
+      console.log(this.pickedObject.name)
+
+      this.pickedObjectSavedColor = this.pickedObject.material.emissive.getHex();
+      // set its emissive color to flashing red/yellow
+      // this.pickedObject.material.emissive.setHex(0xE7504E);
+
+
     }
   }
+}
 const pickHelper = new PickHelper();
 
-const controls = new OrbitControls( camera, renderer.domElement );
-// const pointer = new THREE.Vector2();
-// const raycaster = new THREE.Raycaster();
+const controls = new OrbitControls(camera, labelRenderer.domElement);
+
 const lights = new THREE.SpotLight(0xffffff, 3, 100, 0.2, 0.5, 0.1);
 const lights2 = new THREE.SpotLight(0xffffff, 3, 100, 0.2, 0.5, 0.1);
 const lights3 = new THREE.SpotLight(0xffffff, 3, 100, 0.2, 0.5, 0.1);
 const lights4 = new THREE.SpotLight(0xffffff, 3, 100, 0.2, 0.5, 0.1);
 const loader = new GLTFLoader();
 const helper = new THREE.DirectionalLightHelper(lights, 5);
-camera.position.set(4, 5, 11 );
+camera.position.set(4, 5, 11);
 camera.lookAt(0, 0, 0);
 controls.update();
 
@@ -124,34 +149,34 @@ scene.add(helper);
 
 
 
-  const pickPosition = {x: 0, y: 0};
+const pickPosition = { x: 0, y: 0 };
 clearPickPosition();
- 
 
- 
+
+
 function getCanvasRelativePosition(event) {
   const rect = canvas.getBoundingClientRect();
 
-  const x = (event.clientX - rect.left) * canvas.width  / rect.width
-  const y = (event.clientY - rect.top ) * canvas.height / rect.height
-//   console.log("x:", x)
-//   console.log("y:", y)
+  const x = (event.clientX - rect.left) * canvas.width / rect.width
+  const y = (event.clientY - rect.top) * canvas.height / rect.height
+  //   console.log("x:", x)
+  //   console.log("y:", y)
   return {
     x,
     y,
   };
 }
- 
+
 function setPickPosition(event) {
   const pos = getCanvasRelativePosition(event);
-  pickPosition.x = (pos.x / canvas.width ) *  2 - 1;
+  pickPosition.x = (pos.x / canvas.width) * 2 - 1;
   pickPosition.y = (pos.y / canvas.height) * -2 + 1;  // note we flip Y
 
-//   console.log(pickPosition)
-//   console.log("canvas.height)", canvas.height)
+  //   console.log(pickPosition)
+  //   console.log("canvas.height)", canvas.height)
   //   console.log("y:", y)
 }
- 
+
 function clearPickPosition() {
   // unlike the mouse which always has a position
   // if the user stops touching the screen we want
@@ -159,46 +184,52 @@ function clearPickPosition() {
   // unlikely to pick something
   pickPosition.x = -100000;
   pickPosition.y = -100000;
+
+  if (pickHelper && pickHelper.molekulaDiv)
+  {
+    console.log("clearPickPosition")
+    pickHelper.molekulaDiv.remove();
+    pickHelper.molekulaLabel.removeFromParent();
+  }
 }
- 
+
 window.addEventListener('mousemove', setPickPosition);
 window.addEventListener('mouseout', clearPickPosition);
 window.addEventListener('mouseleave', clearPickPosition);
 
-loader.load( './Blender/PAN_TIMUR_GENIUS.glb', async function ( gltf ) {
-    const model = gltf.scene;
-    model.position.set(0, 0, 0);
-	scene.add( model );
+// loader.load( './Blender/PAN_TIMUR_GENIUS.glb', async function ( gltf ) {
+//     const model = gltf.scene;
+//     model.position.set(0, 0, 0);
+//   scene.add( model );
 
-    renderer.render( scene, camera, model );
-}, undefined, function ( error ) {
+//     renderer.render( scene, camera, model );
+// }, undefined, function ( error ) {
 
-	console.error( error );
+//   console.error( error );
 
-} );
+// } );
 
-loader.load( './Blender/PAN_TIMUR_GENIUS.glb', async function ( gltf ) {
-    const model = gltf.scene;
-    model.position.set(10, 0, 0);
-	scene.add( model );
-    
-    renderer.render( scene, camera, model );
-}, undefined, function ( error ) {
-	console.error( error );
-} );
-function animate(time){
-    controls.update();
-    renderer.render( scene, camera );
-    time *= 0.001;
-    pickHelper.pick(pickPosition, scene, camera, time);
+loader.load('https://storage.googleapis.com/metachondria-model/final2).glb', async function (gltf) {
+  const model = gltf.scene;
+  model.position.set(5, -8, 0);
+  scene.add(model);
+
+  renderer.render(scene, camera, model);
+}, undefined, function (error) {
+  console.error(error);
+});
+function animate(time) {
+  controls.update();
+  renderer.render(scene, camera);
+  labelRenderer.render( scene, camera );
+  time *= 0.001;
+  pickHelper.pick(pickPosition, scene, camera, time);
+  
 }
+
 renderer.outputColorSpace = THREE.SRGBColorSpace
-renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setClearColor(0xffffff);
 renderer.setPixelRatio(window.devicePixelRatio);
-document.body.appendChild( renderer.domElement );
-renderer.setAnimationLoop( animate );
-// stats = new Stats();
-// 				document.body.appendChild( stats.dom );
-				// document.addEventListener( 'mousemove', onPointerMove );
-				// window.addEventListener( 'resize', onWindowResize );
+document.body.appendChild(renderer.domElement);
+renderer.setAnimationLoop(animate);
